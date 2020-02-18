f730aa344aca51919c3d1ac4cff60bc89724d32a
Marc Erasmo <mde9259@gmail.com>
2020-02-18 10:45:51 -0500

    added user connected and disconnected msg

--- FileMSG/server.js
+++ FileMSG/server.js
@@ -8,8 +8,13 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
+  console.log('Yay, connection was recorded')
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
+    io.emit('chat message', 'A user is connected');
+    socket.on('disconnect', function() {
+      io.emit('chat message', 'some user disconnected');
+   });
   });
 });
 
